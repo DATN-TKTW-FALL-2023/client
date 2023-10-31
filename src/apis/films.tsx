9f6e6a9aa10bb8be21129c72db93ed0d6bbebaaf
rynddev <manhnvph25559@fpt.edu.vn>
2023-10-31 14:04:47 +0700

    feat: api_get_list_film

---
+++ src/apis/films.tsx
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { pause } from '@/utils/pause';
+
+
+const FilmApi = createApi({
+          reducerPath: 'film',
+          tagTypes: ['Film'],
+          baseQuery: fetchBaseQuery({
+              baseUrl: "http://45.95.173.37:6500/api",
+              fetchFn: async (...args) => {
+                  await pause(1000);
+                  return fetch(...args);
+              }
+          }),
+          endpoints: (builder) => ({
+              getFilms: builder.query({
+                  query: () => `/v1/film/client`,
+                  providesTags: ['Film']
+              }),
+              getFilmsById: builder.query({
+                  query: (id) => `/v1/film/${id}/client`,
+                  providesTags: ['Film']
+              })
+             
+          })
+      });
+      
+      export const {
+          useGetFilmsQuery,
+          useGetFilmsByIdQuery
+      } = FilmApi;
+      export const productReducer = FilmApi.reducer;
+      export default FilmApi;
