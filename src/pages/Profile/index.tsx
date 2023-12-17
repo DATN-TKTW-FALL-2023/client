b351dc67b73457050afa3a8c0fe8adc8b0589dae
tonyanh <trunganh123cx@gmail.com>
2023-12-17 22:52:25 +0700

    fix update profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -58,13 +58,13 @@ const Profile = () => {
               <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium font-bold text-black">Họ</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 font-normal sm:col-span-2">
-                  {userData?.data.lastName}
+                  {userData?.data.firstName}
                 </dd>
               </div>
               <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium font-bold text-black">Tên</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 font-normal sm:col-span-2">
-                  {userData?.data.firstName}
+                  {userData?.data.lastName}
                 </dd>
               </div>
               <div className="flex justify-end mx-8">
--- src/pages/Profile/updateProfile.tsx
+++ src/pages/Profile/updateProfile.tsx
@@ -3,8 +3,9 @@ import { useGetUserProfileQuery, useUpdateUserProfileMutation } from "@/apis/use
 import Loading from "@/components/Loading";
 import "../../App.css";
 import { GiTicket } from "react-icons/gi";
-import { Alert, Space, message  } from 'antd';
+import { Alert, Space, message } from "antd";
 import { useNavigate } from "react-router-dom";
+
 const UpdateProfile = () => {
   const { data: userData, isLoading, isError } = useGetUserProfileQuery({});
   const [updateProfileData, setUpdateProfileData] = useState({
@@ -15,17 +16,59 @@ const UpdateProfile = () => {
     lastName: userData?.data.lastName || "",
   });
 
-  const navigate = useNavigate()
-  console.log(updateProfileData);
-  
-  const [updateUserProfile, { isLoading: isUpdating, isError: updateError }] = useUpdateUserProfileMutation();
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    firstName: "",
+    lastName: "",
+  });
 
-  const handleInputChange = (e:any) => {
+  const navigate = useNavigate();
+
+  const [updateUserProfile, { isLoading: isUpdating, isError: updateError }] =
+    useUpdateUserProfileMutation();
+
+  const handleInputChange = (e: any) => {
     const { name, value } = e.target;
     setUpdateProfileData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "",
+    }));
+  };
+
+  const handleUpdateProfile = async () => {
+    const newErrors = {
+      username: !updateProfileData.username ? "Vui lòng nhập username." : "",
+      email: !updateProfileData.email ? "Vui lòng nhập email." : "",
+      phone: !updateProfileData.phone ? "Vui lòng nhập phone." : "",
+      firstName: !updateProfileData.firstName ? "Vui lòng nhập họ." : "",
+      lastName: !updateProfileData.lastName ? "Vui lòng nhập tên." : "",
+    };
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (updateProfileData.email && !emailRegex.test(updateProfileData.email)) {
+      newErrors.email = "Email không hợp lệ.";
+    }
+
+    setErrors(newErrors);
+
+    if (Object.values(newErrors).some((error) => error !== "")) {
+      return;
+    }
+
+    try {
+      const response = await updateUserProfile(updateProfileData).unwrap();
+      message.success("Cập nhật thông tin thành công");
+      navigate("/profile");
+    } catch (error) {
+      console.error("Error updating profile", error);
+      message.error("Có lỗi xảy ra khi cập nhật thông tin");
+    }
   };
 
   useEffect(() => {
@@ -38,17 +81,12 @@ const UpdateProfile = () => {
     });
   }, [userData]);
 
-  const handleUpdateProfile = async () => {
-    try {
-      const response = await updateUserProfile(updateProfileData).unwrap();
-      message.success(`Cập nhật thông tin thành công`);
-      navigate("/profile");
-    } catch (error) {
-      console.error("Error updating profile", error);
-    }
-  };
   if (isLoading) {
-    return <div className="text-center"><Loading /></div>;
+    return (
+      <div className="text-center">
+        <Loading />
+      </div>
+    );
   }
 
   if (isError) {
@@ -61,7 +99,7 @@ const UpdateProfile = () => {
         <div className="mx-auto max-w-2xl shadow-htr overflow-hidden sm:rounded-lg">
           <div className="px-4 py-5 sm:px-6">
             <h3 className="text-lg leading-6 font-medium text-gray-900">
-              Sửa thông tin tài khoản 
+              Sửa thông tin tài khoản
             </h3>
           </div>
           <div className="border-t border-gray-200">
@@ -78,6 +116,7 @@ const UpdateProfile = () => {
                     value={updateProfileData.username}
                     onChange={handleInputChange}
                   />
+                  {errors.username && <div className="text-red-500">{errors.username}</div>}
                 </dd>
               </div>
               <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -92,6 +131,7 @@ const UpdateProfile = () => {
                     value={updateProfileData.email}
                     onChange={handleInputChange}
                   />
+                  {errors.email && <div className="text-red-500">{errors.email}</div>}
                 </dd>
               </div>
               <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -106,6 +146,7 @@ const UpdateProfile = () => {
                     value={updateProfileData.phone}
                     onChange={handleInputChange}
                   />
+                  {errors.phone && <div className="text-red-500">{errors.phone}</div>}
                 </dd>
               </div>
               <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -120,6 +161,7 @@ const UpdateProfile = () => {
                     value={updateProfileData.firstName}
                     onChange={handleInputChange}
                   />
+                  {errors.firstName && <div className="text-red-500">{errors.firstName}</div>}
                 </dd>
               </div>
               <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -134,6 +176,7 @@ const UpdateProfile = () => {
                     value={updateProfileData.lastName}
                     onChange={handleInputChange}
                   />
+                  {errors.lastName && <div className="text-red-500">{errors.lastName}</div>}
                 </dd>
               </div>
               <div className="m-6">
@@ -148,7 +191,6 @@ const UpdateProfile = () => {
                     </span>
                     {isUpdating ? "Đang cập nhật..." : "Cập nhật thông tin"}
                   </button>
-
                 </div>
               </div>
             </dl>
