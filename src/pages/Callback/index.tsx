a61d0fbb89d1588d49679a4f76dc3dec742df7f8
tonyanh <trunganh123cx@gmail.com>
2023-12-20 22:05:10 +0700

    update bill

--- src/pages/Callback/index.tsx
+++ src/pages/Callback/index.tsx
@@ -48,7 +48,7 @@ const PaymentSuccess = () => {
       createOrdertest();
     }
   }, [vnpayRes]);
-
+console.log(data)
   return (
     <div className="container">
       {isLoadingCreate ? (
@@ -57,8 +57,7 @@ const PaymentSuccess = () => {
         <div className="mt-12 mb-12 px-8 py-4 shadow-htr rounded w-[800px] mx-auto ">
           <div className="grid grid-cols-2 ">
             <div className="flex">
-              <p className="text-2xl font-medium">HÓA ĐƠN </p>
-
+              <p className="text-2xl font-medium">HÓA ĐƠN</p>
               {vnpayRes == "00" ? (
                 <p className=" mt-[3px] mx-4">
                   <span className="px-2 py-[4px] font-medium bg-[#89ce84] text-xs text-white rounded">
@@ -68,7 +67,7 @@ const PaymentSuccess = () => {
               ) : (
                 <p className=" mt-[3px] mx-4">
                   <span className="px-2 py-[4px] font-medium bg-red-500 text-xs text-white rounded">
-                    HỦY THÀNH CÔNG
+                    HỦY THANH TOÁN
                   </span>
                 </p>
               )}
@@ -82,66 +81,84 @@ const PaymentSuccess = () => {
                     : "Invalid Date"}
                 </p>
               </div>
+              <div className="bill ">
+                <p className="font-medium">Mã hóa đơn: </p>
+                <p className="text-[#6a6868]">{orderId}</p>
+              </div>
               <div className="bill">
                 <p className="font-medium">Hình thức thanh toán: </p>
                 <p className="text-[#6a6868]">NCB - Ngân Hàng Quốc Dân</p>
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-2 mt-12">
+      
+          {vnpayRes == "00" ? (
             <div>
-              <p className="font-medium text-xl">Nhà cung cấp dịch vụ: </p>
-              <p className="text-base mt-4">MOFY BOOKING COMPANY LTD</p>
-            </div>
-            <div>
-              <p className="font-medium text-xl">Thông tin khách hàng: </p>
-              <div className="mt-4">
-                <p className="text-base">{data?.data?.user?.lastName}</p>
-                <p className="text-base">{data?.data?.user?.phone}</p>
-                <p className="text-base">{data?.data?.user?.email}</p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div>
-              <p className="font-medium text-xl">Chi tiết đơn hàng: </p>
-            </div>
-            <div>
-              <div className="mt-4 border-b-2 border-[#ccc] flex justify-between pr-8 pb-2">
-                <p className="text-[#747373]">Tên Film</p>
-                <p className="text-[#747373]">Phòng</p>
-                <p className="text-[#747373]">Ghế</p>
-                <p className="text-[#747373]">Thành tiền</p>
-              </div>
-              <div className="border-b-2 border-[#ccc] py-4 flex justify-between pr-8">
-                <p>{data?.data?.film}</p>
-                <p>{data?.data?.room}</p>
-                <p>
-                  {" "}
-                  {data?.data?.seats.map((seat: any) => (
-                    <span>{seat} </span>
-                  ))}
-                </p>
-                <p>{data?.data?.price}đ</p>
-              </div>
-              <div className="py-4 flex justify-between pr-8">
-                <div></div>
-                <div className="flex">
-                  <p className="mr-6 font-bold">Tổng tiền thanh toán</p>
-                  <p className="">{data?.data?.price}đ</p>
+              <div className="grid grid-cols-2 mt-12">
+                <div>
+                  <p className="font-medium text-xl">Nhà cung cấp dịch vụ: </p>
+                  <p className="text-base mt-4">MOFY BOOKING COMPANY LTD</p>
+                </div>
+                <div>
+                  <p className="font-medium text-xl">Thông tin khách hàng: </p>
+                  <div className="mt-4">
+                    <p className="text-base">{data?.data?.user?.lastName}</p>
+                    <p className="text-base">{data?.data?.user?.phone}</p>
+                    <p className="text-base">{data?.data?.user?.email}</p>
+                  </div>
                 </div>
               </div>
-              <div className="flex justify-end pr-8 mt-2">
+              <div>
+                <div>
+                  <p className="font-medium text-xl">Chi tiết đơn hàng: </p>
+                </div>
+                <div>
+                  <div className="mt-4 border-b-2 border-[#ccc] flex justify-between pr-8 pb-2">
+                    <p className="text-[#747373]">Tên Film</p>
+                    <p className="text-[#747373]">Phòng</p>
+                    <p className="text-[#747373]">Ghế</p>
+                    <p className="text-[#747373]">Thành tiền</p>
+                  </div>
+                  <div className="border-b-2 border-[#ccc] py-4 flex justify-between pr-8">
+                    <p>{data?.data?.film}</p>
+                    <p>{data?.data?.room}</p>
+                    <p>
+                      {" "}
+                      {data?.data?.seats.map((seat: any) => (
+                        <span>{seat} </span>
+                      ))}
+                    </p>
+                    <p>{data?.data?.price}đ</p>
+                  </div>
+                  <div className="py-4 flex justify-between pr-8">
+                    <div></div>
+                    <div className="flex">
+                      <p className="mr-6 font-bold">Tổng tiền thanh toán</p>
+                      <p className="">{data?.data?.price}đ</p>
+                    </div>
+                  </div>
+                  <div className="flex justify-end pr-8 mt-2">
+                  {/* <Link className="btn w-[18%] text-center text-white font-medium  py-2 rounded-lg" to="/">
+                      <span>
+                        <GiTicket className="bg-icon" />
+                      </span>
+                      Quay lại
+                  </Link> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+              ''
+            )}
+            <div className="flex justify-end mt-8">
               <Link className="btn w-[18%] text-center text-white font-medium  py-2 rounded-lg" to="/">
-               
                   <span>
                     <GiTicket className="bg-icon" />
                   </span>
                   Quay lại
               </Link>
-              </div>
             </div>
-          </div>
         </div>
       )}
     </div>
